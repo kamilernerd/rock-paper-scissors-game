0f9daf3cd035479f4a3827ceac4b1cc56b170757
Kamil Oracz <kamil.oracz98@gmail.com>
2024-10-02 00:18:03 +0200

    Fix issue where spamming buttons would propagate choice into next rounds

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const options = ["rock", "paper", "scissor"];
-let score = {
-  you: 0,
-  computer: 0
+const gameState = {
+	score: {
+		you: 0,
+		computer: 0,
+	},
+	gameIsRunning: false,
 };
 const WIN_GREEN = "#6ac475";
 const LOSE_RED = "#c4736a";
@@ -11,120 +14,127 @@ const playerHand = document.querySelector(".hands .player-hand");
 const computerHand = document.querySelector(".hands .computer-hand");
 
 function getRandomInt() {
-  return Math.floor(Math.random() * 3);
+	return Math.floor(Math.random() * 3);
 }
 
 const btns = document.querySelectorAll(".options button");
 btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(".hands .player-hand").src = "rock.png";
-    document.querySelector(".hands .computer-hand").src = "rock.png";
-    playerHand.classList.add('shakePlayer')
-    computerHand.classList.add('shakeComputer')
-    const playerA = btn.querySelector("label").innerText;
-    const playerB = options[getRandomInt()];
-    setTimeout(()=>compare(playerA, playerB),1000)
-    
-  });
+	btn.addEventListener("click", () => {
+		if (gameState.gameIsRunning) return
+		gameState.gameIsRunning = true;
+
+		document.querySelector(".hands .player-hand").src = "rock.png";
+		document.querySelector(".hands .computer-hand").src = "rock.png";
+		playerHand.classList.add("shakePlayer");
+		computerHand.classList.add("shakeComputer");
+		const playerA = btn.querySelector("label").innerText;
+		const playerB = options[getRandomInt()];
+
+		setTimeout(() => {
+			compare(playerA, playerB);
+		}, 1000);
+	});
 });
 
 function compare(player, computer) {
-  const won = "YOU WON";
-  const lose = "YOU LOST";
-  const resultEl = document.querySelector(".hands .result");
-  const youScore = document.querySelector(".score .you");
-  const computerScore = document.querySelector(".score .computer");
+	const won = "YOU WON";
+	const lose = "YOU LOST";
+	const resultEl = document.querySelector(".hands .result");
+	const youScore = document.querySelector(".score .you");
+	const computerScore = document.querySelector(".score .computer");
 
-  if (player == computer) {
-    console.log(`${player} is equal to ${computer}`);
-    update(player, computer);
-    resultEl.style.color = DRAW_BLUE;
-    resultEl.innerText = "DRAW";
-  } else if (player == "rock" && computer == "scissor") {
-    console.log(`${player} defeats ${computer}`);
-    update(player, computer);
-    score.you++;
-    resultEl.style.color = WIN_GREEN;
-    resultEl.innerText = won;
-    youScore.innerText = score.you;
-  } else if (player == "rock" && computer == "paper") {
-    console.log(`${player} loses to ${computer}`);
-    update(player, computer);
-    score.computer++;
-    resultEl.style.color = LOSE_RED;
-    resultEl.innerText = lose;
-    computerScore.innerText = score.computer;
-  } else if (player == "paper" && computer == "scissor") {
-    console.log(`${player} loses to ${computer}`);
-    update(player, computer);
-    score.computer++;
-    resultEl.style.color = LOSE_RED;
-    resultEl.innerText = lose;
-    computerScore.innerText = score.computer;
-  } else if (player == "paper" && computer == "rock") {
-    console.log(`${player} defeats ${computer}`);
-    update(player, computer);
-    score.you++;
-    resultEl.style.color = WIN_GREEN;
-    resultEl.innerText = won;
-    youScore.innerText = score.you;
-  } else if (player == "scissor" && computer == "rock") {
-    console.log(`${player} loses to ${computer}`);
-    update(player, computer);
-    score.computer++;
-    resultEl.style.color = LOSE_RED;
-    resultEl.innerText = lose;
-    computerScore.innerText = score.computer;
-  } else if (player == "scissor" && computer == "paper") {
-    console.log(`${player} defeats ${computer}`);
-    update(player, computer);
-    score.you++;
-    resultEl.style.color = WIN_GREEN;
-    resultEl.innerText = won;
-    youScore.innerText = score.you;
-  }
-  // playerHand.classList.remove('shake')
-  // computerHand.classList.remove('shake')
+	if (player == computer) {
+		console.log(`${player} is equal to ${computer}`);
+		update(player, computer);
+		resultEl.style.color = DRAW_BLUE;
+		resultEl.innerText = "DRAW";
+	} else if (player == "rock" && computer == "scissor") {
+		console.log(`${player} defeats ${computer}`);
+		update(player, computer);
+		gameState.score.you++;
+		resultEl.style.color = WIN_GREEN;
+		resultEl.innerText = won;
+		youScore.innerText = gameState.score.you;
+	} else if (player == "rock" && computer == "paper") {
+		console.log(`${player} loses to ${computer}`);
+		update(player, computer);
+		gameState.score.computer++;
+		resultEl.style.color = LOSE_RED;
+		resultEl.innerText = lose;
+		computerScore.innerText = gameState.score.computer;
+	} else if (player == "paper" && computer == "scissor") {
+		console.log(`${player} loses to ${computer}`);
+		update(player, computer);
+		gameState.score.computer++;
+		resultEl.style.color = LOSE_RED;
+		resultEl.innerText = lose;
+		computerScore.innerText = gameState.score.computer;
+	} else if (player == "paper" && computer == "rock") {
+		console.log(`${player} defeats ${computer}`);
+		update(player, computer);
+		gameState.score.you++;
+		resultEl.style.color = WIN_GREEN;
+		resultEl.innerText = won;
+		youScore.innerText = gameState.score.you;
+	} else if (player == "scissor" && computer == "rock") {
+		console.log(`${player} loses to ${computer}`);
+		update(player, computer);
+		gameState.score.computer++;
+		resultEl.style.color = LOSE_RED;
+		resultEl.innerText = lose;
+		computerScore.innerText = gameState.score.computer;
+	} else if (player == "scissor" && computer == "paper") {
+		console.log(`${player} defeats ${computer}`);
+		update(player, computer);
+		gameState.score.you++;
+		resultEl.style.color = WIN_GREEN;
+		resultEl.innerText = won;
+		youScore.innerText = gameState.score.you;
+	}
+
+	gameState.gameIsRunning = false;
+	// playerHand.classList.remove('shake')
+	// computerHand.classList.remove('shake')
 }
 function update(player, computer) {
-  const rock = "rock.png";
-  const paper = "paper.png";
-  const scissor = "scissor.png";
+	const rock = "rock.png";
+	const paper = "paper.png";
+	const scissor = "scissor.png";
 
-  // playerHand
-  if (player == "rock") {
-    playerHand.src = rock;
-  } else if (player == "paper") {
-    playerHand.src = paper;
-  } else if (player == "scissor") {
-    playerHand.src = scissor;
-  }
+	// playerHand
+	if (player == "rock") {
+		playerHand.src = rock;
+	} else if (player == "paper") {
+		playerHand.src = paper;
+	} else if (player == "scissor") {
+		playerHand.src = scissor;
+	}
 
-  // computerHand
-  if (computer == "rock") {
-    computerHand.src = rock;
-  } else if (computer == "paper") {
-    computerHand.src = paper;
-  } else if (computer == "scissor") {
-    computerHand.src = scissor;
-  }
-  playerHand.classList.remove('shakePlayer')
-  computerHand.classList.remove('shakeComputer')
+	// computerHand
+	if (computer == "rock") {
+		computerHand.src = rock;
+	} else if (computer == "paper") {
+		computerHand.src = paper;
+	} else if (computer == "scissor") {
+		computerHand.src = scissor;
+	}
+	playerHand.classList.remove("shakePlayer");
+	computerHand.classList.remove("shakeComputer");
 }
-
 
 //Reset Game Button ---------------------------------
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", () => {
-  score.you = 0;
-  score.computer = 0;
-  document.querySelector(".score .you").innerText = score.you;
-  document.querySelector(".score .computer").innerText = score.computer;
-  document.querySelector(".hands .result").innerText = "";
-  document.querySelector(".hands .player-hand").src = "rock.png";
-  document.querySelector(".hands .computer-hand").src = "rock.png";
+	gameState.score = {
+		you: 0,
+		computer: 0,
+	};
+	document.querySelector(".score .you").innerText = gameState.score.you;
+	document.querySelector(".score .computer").innerText = gameState.score.computer;
+	document.querySelector(".hands .result").innerText = "";
+	document.querySelector(".hands .player-hand").src = "rock.png";
+	document.querySelector(".hands .computer-hand").src = "rock.png";
 });
-
 
 // // Mapping
 // // 0 -> rock, 1->paper, 2->scissor
